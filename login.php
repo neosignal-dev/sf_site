47fabb8f738e1f7b9e5d31f76cf09545de2f2182
Mikhail <40924193+smerzh@users.noreply.github.com>
2025-03-21 18:04:25 +0300

    fix error

--- login.php
+++ login.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <title>CodePen - Glassmorphism login Form Tutorial in html css</title>
-  
 
 </head>
 <body>
@@ -13,7 +12,7 @@
 <head>
   <!-- Design by foolishdeveloper.com -->
     <title>Glassmorphism login Form Tutorial in html css</title>
- 
+
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
@@ -167,6 +166,6 @@ button{
 </body>
 </html>
 <!-- partial -->
-  
+
 </body>
 </html>
